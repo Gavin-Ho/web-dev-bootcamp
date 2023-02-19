72a1f0d0eccd456f988019774121b4862ae30e98
Gavin Ho <111198408+Gavin-Ho@users.noreply.github.com>
2023-02-19 13:11:09 -0800

    Added animations

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -3,6 +3,7 @@ document.addEventListener("keydown", function(event){
         
     console.log(event);
     makeSound(event.key);
+    playAnimation(event.key);
 
 });
 
@@ -17,6 +18,8 @@ for(var i = 0; i < numberDrums; i++){
         var buttonInnerHTML = this.innerHTML;
         
         makeSound(buttonInnerHTML);
+        playAnimation(buttonInnerHTML);
+
 
     });
 }
@@ -67,50 +70,14 @@ function makeSound(key){
 
 }
 
+// Play animation when button is activated
+function playAnimation(currentKey){
+    
+    var activebutton = document.querySelector("." + currentKey);
 
-function handleClick(){
-    var buttonInnerHTML = this.innerHTML;
-
-    switch(buttonInnerHTML){
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-
-        case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-
-        case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-
-        case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-            
-        case "j":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-
-        case "k":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-            
-        case "l":
-            var kick = new Audio("sounds/kick.mp3");
-            kick.play();
-            break;
-
-        default:
-            console.log("buttonInnerHTML");
-            break;
-
-    }
+    activebutton.classList.add("pressed");
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    }, 100);
 
 }
