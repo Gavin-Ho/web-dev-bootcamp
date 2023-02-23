4606ca4057e732f9dd641237d0439d290feb3368
Gavin Ho <111198408+Gavin-Ho@users.noreply.github.com>
2023-02-23 08:50:51 -0800

    Added a BMI calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Addition Calculator
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
@@ -14,6 +15,18 @@ app.post("/", function(req, res){
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
     res.send("The result of the calculation is " + result);
+});
+
+// BMI Calculator
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight/(height*height);
+    res.send("The calculated BMI is " + bmi);
 });
 
 app.listen(3000, function(){
