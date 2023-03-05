97eb38397e1465ce1dc094a26319739272b65844
Gavin Ho <111198408+Gavin-Ho@users.noreply.github.com>
2023-03-04 19:03:09 -0800

    Implemented Mailchimp API

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const request = require('request');
+const https = require('https');
+
 
 const app = express();
 
@@ -16,9 +18,58 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/', function(request, response){
-    var firstName = request.body.firstName;
-    var lastName = request.body.lastName;
-    var email = request.body.email;
+app.post('/', function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
     console.log(firstName, lastName, email);
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            }
+        }]
+    };
+    
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/e05506c057";
+
+    const options = {
+        method: "POST",
+        auth: "gavin1:555478db7841bc90a38ba1c2b400b10b-us11xd",
+    };
+
+    const dataRequest = https.request(url, options, function(response){
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    dataRequest.write(jsonData);
+
+    dataRequest.end();
+
 });
+
+app.post('/failure', function(req, res){
+    res.redirect('/');
+});
+
+
+// API Key
+// 555478db7841bc90a38ba1c2b400b10b-us11
+
+// Audience ID
+// e05506c057
