17691417bcdd8932b47b6306c021dc543d14468b
Gavin Ho <111198408+Gavin-Ho@users.noreply.github.com>
2023-03-11 17:26:06 -0800

    Add styling and update functionality

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,45 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
+let workItems = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
 
 app.get('/', function(request, response){
     
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day ="";
+    let day = date.getDate();
 
-    switch(currentDay){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
-    
-    response.render('list', {kindOfDay: day});
+    response.render('list', {
+        listTitle: day,
+        newListItems: items,
+    });
 
 });
+
+app.post("/", function(request, response){
+    let item = request.body.newItem;
+
+    if(request.body.list === "Work List"){
+        workItems.push(item);
+        response.redirect('/work');
+    }else{
+        items.push(item);
+        response.redirect('/');
+    }
+    
+});
+
+app.get('/work', function(request,response){
+    response.render('list', {
+        listTitle: "Work List",
+        newListItems: workItems,
+    })
+});
+
+app.get('/about', function(request,response){
+    response.render('about');
+})
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
